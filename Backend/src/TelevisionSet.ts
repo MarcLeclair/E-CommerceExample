3b4fed4b8b1245eeaf5d260279f7b346d12d1b69
marc <marc.leclair0113@gmail.com>
2017-10-05 16:55:46 -0400

    -Getters/Setters in ComputerSystem -TelevisionSet Getters/Setters/Delete

--- Backend/src/TelevisionSet.ts
+++ Backend/src/TelevisionSet.ts
@@ -6,13 +6,67 @@ import { Electronic } from "./electronic";
 var db = new dbconnection().getDBConnector();
 
 export  class TelevisionSet extends Electronic{
-    dimensions: string;
-    type: string;
+    private dimensions: string;
+    private type: string;
 
     constructor(id: string, weight: number, modelNumber: string, brand: string, price: number, dimensions: string , type : string) {
         super(id, weight, modelNumber, brand, price,"TelevisionSet");
         this.dimensions = dimensions;
         this.type = type;
+    }
+
+    private setDimensions(dimensions: string) {
+        this.dimensions = dimensions;
+    }
+
+    private setType(type : string) {
+        this.type = type;
+    }
+
+    private setId(id :string) {
+        this.id = id;
+    }
+
+    private setWeight(weight: number) {
+        this.weight = weight;
+    }
+
+    private setModelNumber(modelNumber : string) {
+        this.modelNumber = modelNumber;
+    }
+
+    private setBrand(brand: string) {
+        this.brand = brand;
+    }
+    private setPrice(price: number) {
+        this.price = price;
+    }
+
+    getDimension() : string {
+        return this.dimensions;
+    }
+
+    getType(): string{
+        return this.type;
+    }
+
+    getId() : string {
+        return this.id;
+    }
+
+    getWeight() : number {
+        return this.weight;
+    }
+
+    getModelNumber() : string {
+        return this.modelNumber;
+    }
+
+    getBrand() : string {
+        return this.brand;;
+    }
+    getPrice(): number{
+        return this.price;
     }
 
 
@@ -42,5 +96,22 @@ export  class TelevisionSet extends Electronic{
             });
         return televisionSet;
     }
+
+    public static delete(tv: TelevisionSet): boolean {
+
+        let deletedOrNot: boolean;
+        db.query('Delete * FROM televisionsets WHERE id=' + tv.id + ';', function (err, result) {
+            if (err) {
+                deletedOrNot = false;
+                throw err;
+            }
+            console.log("Number of records deleted: " + result.affectedRows);
+            if (result.length > 0) deletedOrNot = true;
+        });
+
+        return deletedOrNot;
+    }
+
+    
 }
 
--- Backend/src/computersystem.ts
+++ Backend/src/computersystem.ts
@@ -1,14 +1,13 @@
-import {Electronic} from "./electronic";
+import { Electronic } from "./electronic";
 
-export abstract class ComputerSystem extends Electronic{
+export abstract class ComputerSystem extends Electronic {
     processor: string;
     ram: number;
     cpus: number;
     hardDrive: number;
     os: string;
 
-    constructor(id: string, weight: number, modelNumber: string, brand: string, price: number, electronicType: string, processor: string, ram: number,cpus: number, hardDrive: number, os: string) 
-    {
+    constructor(id: string, weight: number, modelNumber: string, brand: string, price: number, electronicType: string, processor: string, ram: number, cpus: number, hardDrive: number, os: string) {
         super(id, weight, modelNumber, brand, price, electronicType);
         this.processor = processor;
         this.ram = ram;
@@ -16,5 +15,17 @@ export abstract class ComputerSystem extends Electronic{
         this.hardDrive = hardDrive;
         this.os = os;
     }
-    abstract save():boolean;
+    abstract save(): boolean;
+
+    protected abstract getProcessor(): string;
+    protected abstract getRam(): number;
+    protected abstract getCpus(): number;
+    protected abstract getHardDrive(): number;
+    protected abstract getOs(): string;
+
+    protected abstract setProcessor(processor: string);
+    protected abstract setRam(ram: number);
+    protected abstract setCpus(cpus: number);
+    protected abstract setHardDrive(hardDrive: number);
+    protected abstract setOs(os: string);
 }
